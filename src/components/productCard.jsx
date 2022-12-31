d6ce0624c4505b89ebad857f2f74070f381efd70
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2022-12-31 00:34:31 +0000

    'commit'

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NOimg = require('../images/noImg.png');
 
 function ProductCard(props) {
+    const [btnON, setbtnON] =useState('')
 
 
+    const setfav =() =>{
+        if (btnON != 'btnON'){
+            setbtnON('btnON')
+        }else{setbtnON('')}
+    }
 
     return ( 
         <>
         <div className='productCard'>
             <div className='PImage'>
-                <button onClick={props.setfav} className={props.toggle}><i className='fa fa-heart'></i></button>
+                <button onClick={setfav} className={btnON}><i className='fa fa-heart'></i></button>
                 <img src={props.postImage} alt='image is loading' />
             </div>      
             <div className='brief'>
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -5,18 +5,13 @@ import axios from 'axios'
 function Products() {
 
     const [product , setproduct] = useState([])
-    const [btnON, setbtnON] =useState('')
     useEffect(() =>{
        axios.get('https://web-production-729b.up.railway.app/api/post/').then((res) =>{
         setproduct(res.data)
     })
     },[])
 
-    const setfav =() =>{
-        if (btnON != 'btnON'){
-            setbtnON('btnON')
-        }else{setbtnON('')}
-    }
+    
     const addcart = (e) =>{
     }
     
@@ -29,8 +24,6 @@ function Products() {
             <ProductCard 
             key={item.id}
             addcart={addcart}
-            setfav={setfav}
-            toggle={btnON}
             postImage={item.postImage}
             postTitle={item.postTitle} 
             postPrice ={item.postPrice}/>
