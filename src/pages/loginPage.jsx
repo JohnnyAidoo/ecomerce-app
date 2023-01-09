88a2c45d7e24e656bf9dfd6d815556362fe63694
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2023-01-09 14:23:22 +0000

    'login added'

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 function LoginPage() {
-    return ( 
+    
+    const [username, setusername] = useState('')
+    const [password, setpassword] = useState('')
+    const [credentials, setcredentials] = useState({
+
+        "username": "",
+        "password": ""
+    })
+    
+    const usernamechange = (e) =>{
+        setusername(e.target.value)
+    }
+    
+    const passwordchange = (e) =>{
+        setpassword(e.target.value)
+    }
+
+    const assignValues = () =>{
+        setcredentials({
+            "username": username,
+            "password":password
+        })   
+    }
+    console.log(credentials)
+    const url = ('http://127.0.0.1:8000/api/login/')
+
+    onsubmit = (e) =>{
+        e.preventDefault()
+        if (username != ''){
+            axios.post(url,credentials).then(data =>{
+                console.log(data)
+            }).catch(err =>{
+                console.log(err)
+            })
+        }
+    }
+
+    return (
         <div className='loginPage'>
             <div className='card'>
                 <form  method="post" className='form'>
                     <h4>Login</h4>
-                    <input placeholder='Username' type="text" name="username" id="username" />
-                    <input placeholder='Password' type="password" name="password" id="password" />
+                    <input placeholder='Username' onChange={usernamechange} type="text" name="username" id="username" />
+                    <input placeholder='Password' onChange={passwordchange} type="password" name="password" id="password" />
                     <button type="submit">Login</button>
                     <p>Not Registered yet? <span><a href="/signup">Sign Up</a></span></p>
                 </form>    
--- src/pages/signUP.jsx
+++ src/pages/signUP.jsx
@@ -1,14 +1,64 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+
+
 
 function SignUpPage() {
+
+    const [username, setusername] = useState('')
+    const [email, setemail] = useState('')
+    const [password, setpassword] = useState('')
+    const [password2, setpassword2] = useState('')
+    const [credentials, setcredentials] = useState({
+
+        "username": "",
+        "email": "",
+        "password": ""
+    })
+    
+    const usernamechange = (e) =>{
+        setusername(e.target.value)
+    }
+    
+    const emailchange = (e) =>{
+        setemail(e.target.value)
+    }
+    const passwordchange = (e) =>{
+        setpassword(e.target.value)
+    }
+
+    const password2change =(e) =>{
+        setpassword2(e.target.value)
+    }
+    const assignValues = () =>{
+        setcredentials({
+            "username": username,
+            "email": email,
+            "password":password
+        })        
+    }
+
+    onsubmit = (e) =>{
+        e.preventDefault()
+        if(password != password2){
+            alert('password does no match')
+        }else{
+            axios.post('http://127.0.0.1:8000/api/register/', credentials).then(info =>{
+                console.log(info)
+            }).catch(err =>{
+                console.log(err)
+            })
+        }
+    }
     return ( 
-        <div className='loginPage'>
+        <div onChange={assignValues} className='loginPage'>
             <div className='card'>
-                <form  method="post" className='form'>
+                <form className='form'>
                     <h4>Sign Up</h4>
-                    <input placeholder='Username' type="text" name="username" id="username" />
-                    <input placeholder='password' type="password" name="password1" id="password1" />
-                    <input placeholder='Re-type password' type="password" name="password2" id="password2" />
+                    <input placeholder='Username' onChange={usernamechange} type="text" name="username" id="username" />
+                    <input placeholder='email' onChange={emailchange} type="email" name="email" id="email" />
+                    <input placeholder='password' onChange={passwordchange} type="password" name="password1" id="password1" />
+                    <input placeholder='Re-type password' onChange={password2change} type="password" name="password2" id="password2" />
                     <button type="submit">SignUp</button>
                     <p>Have Account? <span><a href="/login">Login Up</a></span></p>
                 </form>    
